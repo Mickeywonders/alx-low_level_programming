881090e4c1636cf890c8dfaf36d4a19a0b12ee05
Mickeywonders <chikwadomichael@gmail.com>
2022-06-09 22:45:35 -0700

    last tasks

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -20,18 +20,3 @@ void print_number(int n)
 	else
 		print_integer(n);
 }
-
-/**
- * print_integer - A function to printing n
- * @m: an input unsigned integer
- * Return: Nothing
- */
-{
-	unsigned int i = 1000000000;
-
-	for (; i >= 1; i /= 10)
-		if (m / i != 0)
-		{
-			_putchar((m / i) % 10 + '0');
-		}
-}
